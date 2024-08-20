d0181d803fa3fa63d3eaba274880fb3380121e8d
Parth Umrania <pumrania9@gmail.com>
2024-08-20 12:18:05 +0530

    Header - desktop/large device view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,34 @@
+import { useLocation } from "react-router-dom";
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
+import Button from "./Button";
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    const pathname = useLocation();
+
+    return (
+        <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+            <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+                <a href="#hero" className="block w-[12rem] xl:mr-8">
+                    <img src={brainwave} alt="Brainwave" width={190} height={40} />
+                </a>
+
+                <nav className="hidden fixed top-5 left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                    <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+                        {navigation.map((item) => (
+                            <a href={item.url} key={item.id} className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url == pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/50'} lg:leading-5 lg:hover:text-n-1 xl:px-12 ${item.onlyMobile ? 'lg:hidden' : ''}`}  >
+                                {item.title}
+                            </a>
+                        ))}
+                    </div>
+                </nav>
+                <a href="#signup" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block">New account</a>
+                <Button className="hidden lg:flex" href="#login">
+                    Sign In
+                </Button>
+            </div>
+        </div>
+    )
 }
 
 export default Header
